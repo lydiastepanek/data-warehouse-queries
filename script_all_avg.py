4d056e0acc48815458127364360de28a0627d408
Lydia Stepanek <lydia.stepanek@mongodb.com>
2021-04-21 15:49:55 -0400

    wip

---
+++ script_all_avg.py
@@ -0,0 +1,100 @@
+import os
+import pymongo
+from datetime import datetime
+import pdb
+import pandas as pd
+from bson import json_util, ObjectId
+import json
+import pytz
+import re
+
+
+# Requires the PyMongo package.
+# https://api.mongodb.com/python/current
+
+HOST = os.getenv("MONGODBHOST")
+CLIENT = pymongo.MongoClient(HOST)
+DB = CLIENT["xform_cloud"]
+COLLECTION = DB["cloud_last_ping_serverstatus_doc"]
+
+
+def read_mongodb():
+    """
+    Connect to a MongoDB collection through a Mongo URI
+    and collect information about the collection.
+    """
+    tc_info = dict()
+    with open("output_avg.csv", "w+") as f:
+        cursor = COLLECTION.aggregate(
+            [
+                {
+                    "$match": {
+                        # CLOUD records do not contain sessionCatalogSize, so need to analyze ATLAS only
+                        "groupType": "ATLAS",
+                        "hostInfo.typeName": "REPLICA_PRIMARY",
+                        "last_ping_date": {
+                            "$gte": datetime(2021, 4, 21, 0, 0, 0, tzinfo=pytz.utc)
+                        },
+                        "lastPingData.serverStatus.logicalSessionRecordCache.sessionCatalogSize": {
+                            "$ne": None
+                        },
+                        "$and": [
+                            {
+                                "lastPingData.isMaster.tags.region": {
+                                    "$not": re.compile(r"^US.*")
+                                }
+                            },
+                            {
+                                "lastPingData.isMaster.tags.region": {
+                                    "$not": re.compile(r".*US$")
+                                }
+                            },
+                            {
+                                "lastPingData.isMaster.tags.region": {
+                                    "$not": re.compile(r"^SA_.*")
+                                }
+                            },
+                            {
+                                "lastPingData.isMaster.tags.region": {
+                                    "$not": re.compile(r"^SOUTH_AMERICA_.*")
+                                }
+                            },
+                        ],
+                    }
+                },
+                {
+                    "$addFields": {
+                        "localTime": {
+                            "$toDate": {
+                                "$multiply": [
+                                    {"$toLong": "$lastPingData.serverStatus.localTime"},
+                                    1000,
+                                ]
+                            }
+                        },
+                        "region": "$lastPingData.isMaster.tags.region",
+                        "hostTypeName": "$hostInfo.typeName",
+                        "hostClusterId": "$hostInfo.clusterId",
+                        "sessionCatalogSize": "$lastPingData.serverStatus.logicalSessionRecordCache.sessionCatalogSize",
+                        "activeSessionsCount": "$lastPingData.serverStatus.logicalSessionRecordCache.activeSessionsCount",
+                        "endSessionsCount": "$lastPingData.serverStatus.metrics.commands.endSessions.total_delta",
+                        "residentMemory": "$lastPingData.serverStatus.mem.resident",
+                    }
+                },
+                {
+                    "$group": {
+                        "_id": "$hostTypeName",
+                        "average": {"$avg": "$sessionCatalogSize"},
+                        "stdDev": {"$stdDevPop": "$sessionCatalogSize"},
+                    }
+                },
+            ],
+            cursor={},
+            maxTimeMS=1000000,
+        )
+        df = pd.DataFrame(list(cursor))
+        f.write(df.to_csv())
+
+
+if __name__ == "__main__":
+    read_mongodb()
